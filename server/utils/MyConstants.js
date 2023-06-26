e359eeb9bacb92ee3f2b363c15064a9c29a86c67
snippercomment <minhthuanad2002@gmail.com>
2023-06-26 08:31:06 +0700

    deploy to vercel

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster1.2tkmudm.mongodb.net',
+    DB_USER: 'thuanday',
+    DB_PASS: '0812115359thuan',
+    DB_DATABASE: 'Shoppingonline',
+    EMAIL_USER: 'thuanne157@hotmail.com', // Microsoft mail service
+    EMAIL_PASS: '0812115359thuan',
+    JWT_SECRET: 'minhthuanad2002@gmail.com',
+    JWT_EXPIRES: '31556952000', // in milliseconds
+  };
+  module.exports = MyConstants;

5b6dba8f4db26cb005b73c0ac84466cb40124de7
snippercomment <minhthuanad2002@gmail.com>
2024-03-01 14:18:29 +0700

    update v3

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,6 +1,6 @@
 const MyConstants = {
-    DB_SERVER: 'cluster1.2tkmudm.mongodb.net',
-    DB_USER: 'thuanday',
+    DB_SERVER: 'cluster1.xthf0qa.mongodb.net',
+    DB_USER: 'thuanne',
     DB_PASS: '0812115359thuan',
     DB_DATABASE: 'Shoppingonline',
     EMAIL_USER: 'thuanne157@hotmail.com', // Microsoft mail service
@@ -9,3 +9,5 @@ const MyConstants = {
     JWT_EXPIRES: '31556952000', // in milliseconds
   };
   module.exports = MyConstants;
+
+  //mongodb+srv://thuanne:0812115359thuan@cluster1.xthf0qa.mongodb.net/
